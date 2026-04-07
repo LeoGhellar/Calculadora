988d4674b90a3dc9b14e8ec90cee05cdc400ef2f
Leonardo <ghellarleo@gmail.com>
2026-04-07 19:56:35 -0300

    tirei o comentario

--- atividade.js
+++ atividade.js
@@ -1,22 +1,4 @@
 
-/**
-var peso = prompt("Qual o seu peso?");
-var altura = prompt ("Quanto vc mede?");
-var IMC = peso / (altura * altura);
-if (IMC >= 25) 
-  {alert("Você está acima do peso");
-} else {
- alert("Vc não está acima do peso")}
-
- var idade = prompt("Quantos anos você tem?")
- var temCarteira = prompt("Você possui carteira de motorista?(responda com sim ou não")
- if (idade >= 18 && temCarteira == "sim") {
-    alert("Pode dirigir");
-}
-else {alert("Você não pode dirigir")}
-
-**/
-
 var button = document.querySelector("button");
 
 
